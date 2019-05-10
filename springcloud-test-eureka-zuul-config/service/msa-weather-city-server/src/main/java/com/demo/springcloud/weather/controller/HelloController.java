4e019d262abfc006d8eeaa403013a1504a6965d4
yaoyanmeia <yaoyanmeia@enn.cn>
2019-05-10 08:50:20 +0800

    集成config组件

--- springcloud-test-eureka-zuul-config/service/msa-weather-city-server/src/main/java/com/demo/springcloud/weather/controller/HelloController.java
+++ springcloud-test-eureka-zuul-config/service/msa-weather-city-server/src/main/java/com/demo/springcloud/weather/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.demo.springcloud.weather.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-
+	@Value("${hello}")
+	private String hello;
 	@RequestMapping("/hello")
 	public String hello() {
-	    return "Hello World! Welcome to visit waylau.com!";
+	    return hello;
 	}
 
 }
